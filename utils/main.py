7a3b458d36daaeb0651639542d1d92e14c95215d
Mananidipesh <dipeshmanani99@gmail.com>
2024-09-11 17:20:32 +0530

    commit-1

---
+++ utils/main.py
@@ -0,0 +1,17 @@
+from utils.addition import add
+from utils.subtraction import sub
+from utils.multiplication import mult
+from utils.division import div
+
+
+adding = add(3,5)
+print(f"addition : {adding}")
+
+subtract = sub(4,2)
+print(f"subtraction : {subtract}")
+
+multiplicate = mult(4,5)
+print(f"multiplication : {multiplicate}")
+
+divide = div(10,2)
+print(f"division : {divide}")
